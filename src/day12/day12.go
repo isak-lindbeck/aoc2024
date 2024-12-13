738b717a57dda223f4e7dea378b50eb8549a6f0c
Isak Lindbeck <isak.lindbeck@evinova.com>
2024-12-13 09:06:03 +0100

    Day12 cleanup

--- src/day12/day12.go
+++ src/day12/day12.go
@@ -11,24 +11,22 @@ func Run(input string) (int, int) {
 	matrix := utils.RuneMatrix(input)
 	visited := utils.NewMatrixWithDefaultValue(matrix.Width, matrix.Height, false)
 	for x, y := range matrix.Keys() {
-		edges, area, corners := visitPlot(matrix, visited, x, y)
-		ans1 += edges * area
-		ans2 += corners * area
+		region := visitPlot(matrix, visited, x, y)
+		ans1 += region.area * region.edges
+		ans2 += region.area * region.corners
 	}
 
 	return ans1, ans2
 }
 
-func visitPlot(matrix utils.Matrix[rune], visited utils.Matrix[bool], x, y int) (int, int, int) {
+func visitPlot(matrix utils.Matrix[rune], visited utils.Matrix[bool], x, y int) Region {
 	if visited.Get(x, y) {
-		return 0, 0, 0
+		return Region{0, 0, 0}
 	}
 	visited.Set(x, y, true)
-	current := matrix.Get(x, y)
-	edges := 0
-	area := 1
-	corners := 0
+	region := Region{0, 1, 0}
 
+	current := matrix.Get(x, y)
 	// Orthogonal
 	hasUpNeighbour := matrix.GetSafe(x, y-1, '.') == current    // ↑
 	hasDownNeighbour := matrix.GetSafe(x, y+1, '.') == current  // ↓
@@ -40,37 +38,23 @@ func visitPlot(matrix utils.Matrix[rune], visited utils.Matrix[bool], x, y int) 
 	hasDownLeftNeighbour := matrix.GetSafe(x-1, y+1, '.') == current  // ↙
 	hasUpLeftNeighbour := matrix.GetSafe(x-1, y-1, '.') == current    // ↖
 
-	if !hasUpNeighbour {
-		edges++
-	} else {
-		neighboursEdges, neighboursArea, neighboursCorners := visitPlot(matrix, visited, x, y-1)
-		edges += neighboursEdges
-		area += neighboursArea
-		corners += neighboursCorners
+	if hasUpNeighbour {
+		region.merge(visitPlot(matrix, visited, x, y-1))
 	}
-	if !hasDownNeighbour {
-		edges++
-	} else {
-		neighboursEdges, neighboursArea, neighboursCorners := visitPlot(matrix, visited, x, y+1)
-		edges += neighboursEdges
-		area += neighboursArea
-		corners += neighboursCorners
+	if hasDownNeighbour {
+		region.merge(visitPlot(matrix, visited, x, y+1))
 	}
-	if !hasLeftNeighbour {
-		edges++
-	} else {
-		neighboursEdges, neighboursArea, neighboursCorners := visitPlot(matrix, visited, x-1, y)
-		edges += neighboursEdges
-		area += neighboursArea
-		corners += neighboursCorners
+	if hasLeftNeighbour {
+		region.merge(visitPlot(matrix, visited, x-1, y))
 	}
-	if !hasRightNeighbour {
-		edges++
-	} else {
-		neighboursEdges, neighboursArea, neighboursCorners := visitPlot(matrix, visited, x+1, y)
-		edges += neighboursEdges
-		area += neighboursArea
-		corners += neighboursCorners
+	if hasRightNeighbour {
+		region.merge(visitPlot(matrix, visited, x+1, y))
+	}
+
+	for _, hasNeighbour := range []bool{hasUpNeighbour, hasDownNeighbour, hasLeftNeighbour, hasRightNeighbour} {
+		if !hasNeighbour {
+			region.edges++
+		}
 	}
 
 	cornerCases := []bool{
@@ -87,9 +71,19 @@ func visitPlot(matrix utils.Matrix[rune], visited utils.Matrix[bool], x, y int) 
 	}
 	for _, cornerCase := range cornerCases {
 		if cornerCase {
-			corners++
+			region.corners++
 		}
 	}
 
-	return edges, area, corners
+	return region
+}
+
+type Region struct {
+	edges, area, corners int
+}
+
+func (r *Region) merge(other Region) {
+	r.edges += other.edges
+	r.area += other.area
+	r.corners += other.corners
 }
