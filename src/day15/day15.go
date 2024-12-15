c4b06be2bd92a75793cd781bf7ef55c63cc3786b
Isak Lindbeck <isak.lindbeck@evinova.com>
2024-12-15 17:20:40 +0100

    Day15 optimization

--- src/day15/day15.go
+++ src/day15/day15.go
@@ -5,10 +5,10 @@ import (
 	"strings"
 )
 
-var up = Instruction{0, -1}
-var down = Instruction{0, 1}
-var left = Instruction{-1, 0}
-var right = Instruction{1, 0}
+var up = Vector{0, -1}
+var down = Vector{0, 1}
+var left = Vector{-1, 0}
+var right = Vector{1, 0}
 
 func Run(input string) (int, int) {
 	ans1 := 0
@@ -17,83 +17,33 @@ func Run(input string) (int, int) {
 	split := strings.Split(input, "\n\n")
 
 	instructionString := strings.ReplaceAll(split[1], "\n", "")
-	instructions := make([]Instruction, len(instructionString))
+	instructions := make([]Vector, len(instructionString))
 	for i, r := range instructionString {
 		instructions[i] = toInstruction(r)
 	}
 
-	matrix := utils.RuneMatrix(split[0])
-	atX, atY := matrix.GetCoordinates('@')
-	matrix.Set(atX, atY, '.')
-
-	for _, instr := range instructions {
-		newMatrix := utils.CloneMatrix(matrix)
-		if push(&newMatrix, instr, atX+instr.x, atY+instr.y) {
-			atX += instr.x
-			atY += instr.y
-			matrix = newMatrix
-		}
-	}
-	ans1 = sumGps(matrix)
+	ans1 = calculate(split[0], instructions)
 
 	m := strings.ReplaceAll(split[0], ".", "..")
 	m = strings.ReplaceAll(m, "#", "##")
 	m = strings.ReplaceAll(m, "O", "[]")
 	m = strings.ReplaceAll(m, "@", "@.")
-	matrix = utils.RuneMatrix(m)
-	atX, atY = matrix.GetCoordinates('@')
-	matrix.Set(atX, atY, '.')
-	for _, instr := range instructions {
-		newMatrix := utils.CloneMatrix(matrix)
-		if push(&newMatrix, instr, atX+instr.x, atY+instr.y) {
-			atX += instr.x
-			atY += instr.y
-			matrix = newMatrix
-		}
-	}
-	ans2 = sumGps(matrix)
+	ans2 = calculate(m, instructions)
 
 	return ans1, ans2
 }
 
-func push(matrix *utils.Matrix[rune], dir Instruction, x, y int) bool {
-	tile := matrix.Get(x, y)
-	if tile == '#' {
-		return false
-	}
-	if tile == '.' {
-		return true
-	}
-	isVertical := dir == up || dir == down
-	nx := x + dir.x
-	ny := y + dir.y
-	if isVertical && tile == '[' {
-		if push(matrix, dir, nx+1, ny) {
-			matrix.Set(nx+1, ny, ']')
-			matrix.Set(x+1, y, '.')
-		} else {
-			return false
+func calculate(input string, instructions []Vector) int {
+	matrix := utils.RuneMatrix(input)
+	atX, atY := matrix.GetCoordinates('@')
+	matrix.Set(atX, atY, '.')
+	for _, instr := range instructions {
+		if canPush(&matrix, instr, atX+instr.x, atY+instr.y) {
+			push(&matrix, instr, atX+instr.x, atY+instr.y)
+			atX += instr.x
+			atY += instr.y
 		}
 	}
-	if isVertical && tile == ']' {
-		if push(matrix, dir, nx-1, ny) {
-			matrix.Set(nx-1, ny, '[')
-			matrix.Set(x-1, y, '.')
-		} else {
-			return false
-		}
-	}
-
-	if push(matrix, dir, nx, ny) {
-		matrix.Set(nx, ny, tile)
-		matrix.Set(x, y, '.')
-		return true
-	} else {
-		return false
-	}
-}
-
-func sumGps(matrix utils.Matrix[rune]) int {
 	sum := 0
 	for x, y := range matrix.Keys() {
 		if matrix.Get(x, y) == 'O' || matrix.Get(x, y) == '[' {
@@ -103,7 +53,54 @@ func sumGps(matrix utils.Matrix[rune]) int {
 	return sum
 }
 
-func toInstruction(r int32) Instruction {
+func canPush(matrix *utils.Matrix[rune], dir Vector, x, y int) bool {
+	tile := matrix.Get(x, y)
+	if tile == '#' {
+		return false
+	}
+	if tile == '.' {
+		return true
+	}
+	nextX := x + dir.x
+	nextY := y + dir.y
+	if dir.x == 0 && tile == '[' {
+		if !canPush(matrix, dir, nextX+1, nextY) {
+			return false
+		}
+	}
+	if dir.x == 0 && tile == ']' {
+		if !canPush(matrix, dir, nextX-1, nextY) {
+			return false
+		}
+	}
+	return canPush(matrix, dir, nextX, nextY)
+}
+
+func push(matrix *utils.Matrix[rune], dir Vector, x, y int) {
+	tile := matrix.Get(x, y)
+	if tile == '#' || tile == '.' {
+		return
+	}
+	nextX := x + dir.x
+	nextY := y + dir.y
+	if dir.x == 0 && tile == '[' {
+		push(matrix, dir, nextX+1, nextY)
+		matrix.Set(nextX+1, nextY, ']')
+		matrix.Set(x+1, y, '.')
+
+	}
+	if dir.x == 0 && tile == ']' {
+		push(matrix, dir, nextX-1, nextY)
+		matrix.Set(nextX-1, nextY, '[')
+		matrix.Set(x-1, y, '.')
+	}
+
+	push(matrix, dir, nextX, nextY)
+	matrix.Set(nextX, nextY, tile)
+	matrix.Set(x, y, '.')
+}
+
+func toInstruction(r rune) Vector {
 	switch r {
 	case '^':
 		return up
@@ -118,4 +115,9 @@ func toInstruction(r int32) Instruction {
 	}
 }
 
-type Instruction struct{ x, y int }
+type Vector struct{ x, y int }
+
+func (v1 *Vector) move(v2 Vector) {
+	v1.x += v2.x
+	v1.y += v2.y
+}
