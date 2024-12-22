30a2d04fe537177ace6fe61ed590e0ba787f96b1
Isak Lindbeck <isak.lindbeck@evinova.com>
2024-12-22 17:40:15 +0100

    Dat06 optimization

--- src/day06/day06.go
+++ src/day06/day06.go
@@ -4,62 +4,69 @@ import (
 	. "github.com/isak-lindbeck/aoc2024/src/utils"
 )
 
+var directions = []Vector{
+	Up,
+	Right,
+	Down,
+	Left,
+}
+
 func Run(input string) (int, int) {
 	ans1 := 0
 	ans2 := 0
 
-	directions := []Vector{
-		{0, -1}, // ↑
-		{1, 0},  // →
-		{0, 1},  // ↓
-		{-1, 0}, // ←
-	}
 	matrix := RuneMatrix(input)
 	currentDirection := 0
-	startX, startY := matrix.GetCoordinates('^')
+	start := NewVector(matrix.GetCoordinates('^'))
 	var visited = make(map[Vector]bool)
-	currentPos := Vector{X: startX, Y: startY}
-	for matrix.GetSafe(currentPos.X, currentPos.Y, 'X') != 'X' {
+	currentPos := start
+	for matrix.GetSafeAt(currentPos.XY())('X') != 'X' {
 		visited[currentPos] = true
-		next := matrix.GetSafe(currentPos.X+directions[currentDirection].X, currentPos.Y+directions[currentDirection].Y, 'X')
-		if next == '#' {
+		nextPos := currentPos.Add(directions[currentDirection].XY())
+		if matrix.GetSafeAt(nextPos.XY())('X') == '#' {
 			currentDirection = (currentDirection + 1) % 4
 		} else {
-			currentPos = currentPos.Add(directions[currentDirection].XY())
+			currentPos = nextPos
 		}
 
 	}
 	ans1 = len(visited)
 
 	regularPath := visited
-	delete(regularPath, Vector{startX, startY})
+	delete(regularPath, start)
 
-	for blockPos := range regularPath {
-		matrix.Set(blockPos.X, blockPos.Y, '#')
-
-		currentDirection = 0
-		visitedWithDirection := NewMatrixWithDefaultValue(matrix.Width, matrix.Height, 0)
-
-		currentPos = Vector{X: startX, Y: startY}
-		for matrix.GetSafe(currentPos.X, currentPos.Y, 'X') != 'X' {
-			visitedBitFlags := visitedWithDirection.Get(currentPos.X, currentPos.Y)
-			if visitedBitFlags&(1<<currentDirection) > 0 {
-				ans2++
-				break
-			} else {
-				visitedWithDirection.Set(currentPos.X, currentPos.Y, visitedBitFlags|(1<<currentDirection))
-			}
-
-			next := matrix.GetSafe(currentPos.X+directions[currentDirection].X, currentPos.Y+directions[currentDirection].Y, 'X')
-			if next == '#' {
-				currentDirection = (currentDirection + 1) % 4
-			} else {
-				currentPos = currentPos.Add(directions[currentDirection].XY())
-			}
-		}
-
-		matrix.Set(blockPos.X, blockPos.Y, '.')
+	c := make(chan int)
+	for block := range regularPath {
+		go calculateBlockedPosition(matrix, start, block, c)
+	}
+	for range regularPath {
+		ans2 += <-c
 	}
 
 	return ans1, ans2
+}
+
+func calculateBlockedPosition(matrix Matrix[rune], start Vector, block Vector, c chan int) {
+	sum := 0
+	visitedWithDirection := NewMatrixWithDefaultValue(matrix.Width, matrix.Height, uint8(0))
+
+	currentDirection := 0
+	currentPos := start
+	for matrix.GetSafeAt(currentPos.XY())('X') != 'X' {
+		visitedBitFlags := visitedWithDirection.Get(currentPos.X, currentPos.Y)
+		dirBitFlag := uint8(1) << currentDirection
+		if (visitedBitFlags & dirBitFlag) > 0 {
+			sum++
+			break
+		} else {
+			visitedWithDirection.SetAt(currentPos.X, currentPos.Y)(visitedBitFlags | dirBitFlag)
+		}
+		nextPos := currentPos.Add(directions[currentDirection].XY())
+		if matrix.GetSafeAt(nextPos.XY())('X') == '#' || nextPos == block {
+			currentDirection = (currentDirection + 1) % 4
+		} else {
+			currentPos = nextPos
+		}
+	}
+	c <- sum
 }
