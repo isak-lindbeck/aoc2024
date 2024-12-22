13b042af33614be0d9f01a6e99845569c2f401b2
Isak Lindbeck <isak.lindbeck@evinova.com>
2024-12-22 15:25:39 +0100

    Day22 speed up

--- src/day22/day22.go
+++ src/day22/day22.go
@@ -6,6 +6,7 @@ import (
 )
 
 const side4dSlice = 19 // Covers values -9 -> +9
+var sequenceSums = create4dSlice[int]()
 
 func Run(input string) (int, int) {
 	ans1 := 0
@@ -13,34 +14,30 @@ func Run(input string) (int, int) {
 
 	split := strings.Split(input, "\n")
 
-	sequenceSum := create4dSlice(0)
 	for _, s := range split {
 		secretNumber := ints.Parse(s)
 		sequence := []int{0, 0, 0, 0}
-		idx := 0
-		isSet := create4dSlice(false)
+		sequenceOffset := 0
+		isSet := create4dSlice[bool]()
 		for unused := 0; unused < 2000; unused++ {
 			next := processNumber(secretNumber)
 			nexVal := next % 10
 			change := nexVal - (secretNumber % 10)
-			sequence[idx%4] = change
-			if idx > 3 {
-				i := sequence[(idx+0)%4] + 9
-				j := sequence[(idx+1)%4] + 9
-				k := sequence[(idx+2)%4] + 9
-				l := sequence[(idx+3)%4] + 9
-				if !isSet[i][j][k][l] {
-					sequenceSum[i][j][k][l] += nexVal
-					isSet[i][j][k][l] = true
+			sequence[sequenceOffset%4] = change
+			if sequenceOffset > 3 {
+				idx := toIdx(sequence, sequenceOffset)
+				if !isSet[idx] {
+					sequenceSums[idx] += nexVal
+					isSet[idx] = true
 				}
 			}
-			idx++
+			sequenceOffset++
 			secretNumber = next
 		}
 
 		ans1 += secretNumber
 	}
-	ans2 = getMaxValue(sequenceSum)
+	ans2 = getMaxValue(sequenceSums)
 
 	return ans1, ans2
 }
@@ -52,35 +49,27 @@ func processNumber(secretNumber int) int {
 	return secretNumber
 }
 
-func create4dSlice[T any](defaultValue T) [][][][]T {
-	count := make([][][][]T, side4dSlice)
-	for i := range side4dSlice {
-		count[i] = make([][][]T, side4dSlice)
-		for j := range side4dSlice {
-			count[i][j] = make([][]T, side4dSlice)
-			for k := range side4dSlice {
-				count[i][j][k] = make([]T, side4dSlice)
-				for l := range side4dSlice {
-					count[i][j][k][l] = defaultValue
-				}
-			}
-		}
-	}
-	return count
+func create4dSlice[T any]() []T {
+	return make([]T, side4dSlice*side4dSlice*side4dSlice*side4dSlice)
 }
 
-func getMaxValue(count [][][][]int) int {
+func getMaxValue(count []int) int {
 	maxVal := 0
-	for i := range side4dSlice {
-		for j := range side4dSlice {
-			for k := range side4dSlice {
-				for l := range side4dSlice {
-					if count[i][j][k][l] > maxVal {
-						maxVal = count[i][j][k][l]
-					}
-				}
-			}
+	for i := range count {
+		if count[i] > maxVal {
+			maxVal = count[i]
 		}
 	}
 	return maxVal
+}
+
+func toIdx(sequence []int, idx int) int {
+	i := sequence[(idx+0)%4] + 9 // Offset -9 -> 0
+	j := sequence[(idx+1)%4] + 9
+	k := sequence[(idx+2)%4] + 9
+	l := sequence[(idx+3)%4] + 9
+	i = i * side4dSlice * side4dSlice * side4dSlice
+	j = j * side4dSlice * side4dSlice
+	k = k * side4dSlice
+	return i + j + k + l
 }
